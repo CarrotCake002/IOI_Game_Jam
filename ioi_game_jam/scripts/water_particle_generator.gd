51302607a157a6d3d2c1e62f41d5940551fb5e69
cyprien piron <cyprien.piron@gmail.com>
2024-11-30 20:26:24 +0100

    upd: modified water script to work in a scene with default params; moved slyna script to scripts folder

--- ioi_game_jam/scripts/water_particle_generator.gd
+++ ioi_game_jam/scripts/water_particle_generator.gd
@@ -4,10 +4,11 @@ extends Node2D
 @onready var water_particle_scene = preload("res://scenes/water_particle.tscn")
 
 # Define the point where water particles should be spawned
-var spawn_y = 120
-var num_particles = 700
+@export var num_particles = 100
+@export var spawn_interval = 0.01  # Spawn a particle every 0.001 seconds (1 ms)
+@export var spawn_x_range = Vector2(0, 0)
+@export var spawn_y_range = Vector2(0, 0)
 var particles_spawned = 0
-var spawn_interval = 0.003  # Spawn a particle every 0.001 seconds (1 ms)
 var accumulated_time = 0.0
 
 func _ready():
@@ -26,8 +27,8 @@ func _process(delta):
 		accumulated_time -= spawn_interval
 
 func spawn_water_particle():
-	var random_x = randi_range(50, 72)
-	var random_y = randi_range(80, 220)
+	var random_x = randi_range(spawn_x_range.x, spawn_x_range.y)
+	var random_y = randi_range(spawn_y_range.x, spawn_y_range.y)
 	var spawn_point = Vector2(random_x, random_y)
 
 	var water_particle = water_particle_scene.instantiate()
